b8966213ee0278f175c930d41744132614926249
admin <tmongiho@gmail.com>
2017-07-03 11:23:59 +0900

    imageCell 관련 주석 정리

--- ImageCell.swift
+++ ImageCell.swift
@@ -10,19 +10,11 @@ import UIKit
 import Kingfisher
 //import SnapKit
 
-//let isIOS7: Bool = !isIOS8
-//let isIOS8: Bool = floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_7_1
 class ImageCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var imageViewHeightLayoutConstraint: NSLayoutConstraint!
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        //if isIOS7 {
-            // Need set autoresizingMask to let contentView always occupy this view's bounds, key for iOS7
-            //self.contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        //}
-        //self.layer.masksToBounds = true
     }
 
     //TODO : Snapkit을 사용해서 dynamic view 구현..
